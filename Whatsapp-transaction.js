3ee88b0b3742a64373757d386e3a4bd124f9dfda
Yusuf Budiansyah <yusufbbb444@gmail.com>
2024-02-25 19:23:33 +0700

    firstcommit

---
+++ Whatsapp-transaction.js
@@ -0,0 +1,140 @@
+function bayar1(){
+    var nama = document.getElementById('nama1').value;
+    var nomer = document.getElementById('notelp1').value;
+    var asal = document.getElementById('asal1').value;
+
+    var whatsappurl="https://wa.me/6285769791161?text="
+    +"*Nama pemesan kamar          :* "+ nama + "%0a"
+    +"*Nomer telepon                       :* "+ nomer + "%0a"
+    +"*Asal Kota                       :* "+ asal + "%0a"
+    +"*Jumlah yang harus dibayar   :* Rp.300.000"+ "%0a"
+    +"*jenis kamar                            :* Kamar 1";
+
+    window.open(whatsappurl,"_blank").focus();
+}
+function bayar2(){
+    var nama = document.getElementById('nama2').value;
+    var nomer = document.getElementById('notelp2').value;
+    var asal = document.getElementById('asal2').value
+
+    var whatsappurl="https://wa.me/6285769791161?text="
+    +"*Nama pemesan kamar          :* "+ nama + "%0a"
+    +"*Nomer telepon                       :* "+ nomer + "%0a"
+    +"*Asal Kota                       :* "+ asal + "%0a"
+    +"*Jumlah yang harus dibayar   :* Rp.250.000"+ "%0a"
+    +"*jenis kamar                            :* Kamar 1";
+
+    window.open(whatsappurl,"_blank").focus();
+}
+function bayar3(){
+    var nama = document.getElementById('nama3').value;
+    var nomer = document.getElementById('notelp3').value;
+    var asal = document.getElementById('asal3').value
+
+    var whatsappurl="https://wa.me/6285769791161?text="
+    +"*Nama pemesan kamar          :* "+ nama + "%0a"
+    +"*Nomer telepon                       :* "+ nomer + "%0a"
+    +"*Asal Kota                       :* "+ asal + "%0a"
+    +"*Jumlah yang harus dibayar   :* Rp.250.000"+ "%0a"
+    +"*jenis kamar                            :* Kamar 1";
+
+    window.open(whatsappurl,"_blank").focus();
+}
+function bayar4(){
+    var nama = document.getElementById('nama4').value;
+    var nomer = document.getElementById('notelp4').value;
+    var asal = document.getElementById('asal4').value
+
+    var whatsappurl="https://wa.me/6285769791161?text="
+    +"*Nama pemesan kamar          :* "+ nama + "%0a"
+    +"*Nomer telepon                       :* "+ nomer + "%0a"
+    +"*Asal Kota                       :* "+ asal + "%0a"
+    +"*Jumlah yang harus dibayar   :* Rp.300.000"+ "%0a"
+    +"*jenis kamar                            :* Kamar 1";
+
+    window.open(whatsappurl,"_blank").focus();
+}
+function bayar5(){
+    var nama = document.getElementById('nama5').value;
+    var nomer = document.getElementById('notelp5').value;
+    var asal = document.getElementById('asal5').value
+
+    var whatsappurl="https://wa.me/6285769791161?text="
+    +"*Nama pemesan kamar          :* "+ nama + "%0a"
+    +"*Nomer telepon                       :* "+ nomer + "%0a"
+    +"*Asal Kota                       :* "+ asal + "%0a"
+    +"*Jumlah yang harus dibayar   :* Rp.225.000"+ "%0a"
+    +"*jenis kamar                            :* Kamar 1";
+
+    window.open(whatsappurl,"_blank").focus();
+}
+function bayar6(){
+    var nama = document.getElementById('nama6').value;
+    var nomer = document.getElementById('notelp6').value;
+    var asal = document.getElementById('asal6').value
+
+    var whatsappurl="https://wa.me/6285769791161?text="
+    +"*Nama pemesan kamar          :* "+ nama + "%0a"
+    +"*Nomer telepon                       :* "+ nomer + "%0a"
+    +"*Asal Kota                       :* "+ asal + "%0a"
+    +"*Jumlah yang harus dibayar   :* Rp.350.000"+ "%0a"
+    +"*jenis kamar                            :* Kamar 1";
+
+    window.open(whatsappurl,"_blank").focus();
+}
+function bayar7(){
+    var nama = document.getElementById('nama7').value;
+    var nomer = document.getElementById('notelp7').value;
+    var asal = document.getElementById('asal7').value
+
+    var whatsappurl="https://wa.me/6285769791161?text="
+    +"*Nama pemesan kamar          :* "+ nama + "%0a"
+    +"*Nomer telepon                       :* "+ nomer + "%0a"
+    +"*Asal Kota                       :* "+ asal + "%0a"
+    +"*Jumlah yang harus dibayar   :* Rp.350.000"+ "%0a"
+    +"*jenis kamar                            :* Kamar 1";
+
+    window.open(whatsappurl,"_blank").focus();
+}
+function bayar8(){
+    var nama = document.getElementById('nama8').value;
+    var nomer = document.getElementById('notelp8').value;
+    var asal = document.getElementById('asal8').value
+
+    var whatsappurl="https://wa.me/6285769791161?text="
+    +"*Nama pemesan kamar          :* "+ nama + "%0a"
+    +"*Nomer telepon                       :* "+ nomer + "%0a"
+    +"*Asal Kota                       :* "+ asal + "%0a"
+    +"*Jumlah yang harus dibayar   :* Rp.250.000"+ "%0a"
+    +"*jenis kamar                            :* Kamar 1";
+
+    window.open(whatsappurl,"_blank").focus();
+}
+function bayar9(){
+    var nama = document.getElementById('nama9').value;
+    var nomer = document.getElementById('notelp9').value;
+    var asal = document.getElementById('asal9').value
+
+    var whatsappurl="https://wa.me/6285769791161?text="
+    +"*Nama pemesan kamar          :* "+ nama + "%0a"
+    +"*Nomer telepon                       :* "+ nomer + "%0a"
+    +"*Asal Kota                       :* "+ asal + "%0a"
+    +"*Jumlah yang harus dibayar   :* Rp.225.000"+ "%0a"
+    +"*jenis kamar                            :* Kamar 1";
+
+    window.open(whatsappurl,"_blank").focus();
+}
+function bayar10(){
+    var nama = document.getElementById('nama10').value;
+    var nomer = document.getElementById('notelp10').value;
+    var asal = document.getElementById('asal10').value
+
+    var whatsappurl="https://wa.me/6285769791161?text="
+    +"*Nama pemesan kamar          :* "+ nama + "%0a"
+    +"*Nomer telepon                       :* "+ nomer + "%0a"
+    +"*Asal Kota                       :* "+ asal + "%0a"
+    +"*Jumlah yang harus dibayar   :* Rp.200.000"+ "%0a"
+    +"*jenis kamar                            :* Kamar 1";
+
+    window.open(whatsappurl,"_blank").focus();
+}
